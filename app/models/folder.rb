e680e7bfaf4642113f781d82edea5b28563c5478
Kacper Jankowski <kacperjankowski@gmail.com>
2012-05-05 17:32:19 +0200

    Fixed misspelling

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,5 +1,5 @@
 class Folder < ActiveRecord::Base
-  belongs_to :parent, :class_name => "Folder", :foreign_key => "parent_id", 
+  belongs_to :parent, :class_name => "Folder", :foreign_key => "parent_id"
   has_many :subfolders, :class_name => "Folder"
   
   COURSE_LEVEL = "kierunek"
